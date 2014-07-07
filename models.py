7218011ab1032713606767f94ea093d2997ff98d
Brian Tetzlaff <briantetzlaff@gmail.com>
2014-07-07 13:14:11 -0500

    Added possible models.py file to add to

---
+++ models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Models for the NBA app
+
+class Team(models.Model) :
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    #logo
+    #yearsactive
+    #years in playoffs (playoff models)
+    #years of championships (playoff models?)
+    #current coach
+    #current gm
+    #current owner
+    #current roster (player models)
+
+class Player(models.Model) :
+    name = models.CharField(max_length=30)
+    position = models.CharField(max_length=2)
+    education = models.CharField(max_length=200)
+    yearsExp = models.IntegerField(default=0)
+    #separate stats or a list of all of them?
+    #teams played for and years
+    #awards
+    #years went to playoffs (playoff model)
+
+class Year(models.Model) :
+    #all nba 1st team (players)
+    #all nba defensive (players)
+    #finals mvp (player)
+    #champion (team)
