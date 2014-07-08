1d878499470d07ae3d9456fa427c1a39da54c2e1
Brian Tetzlaff <briantetzlaff@gmail.com>
2014-07-08 12:56:11 -0500

    Update models.py

--- models.py
+++ models.py
@@ -4,14 +4,13 @@ from django.db import models
 
 class Team(models.Model) :
     name = models.CharField(max_length=20)
-    location = models.CharField(max_length=20)
+    location = models.CharField(max_length=30)
+    coach = models.CharField(max_length=30)
+    gm = models.CharField(max_length=30)
+    owner = models.CharField(max_length=30)
+    #twitter
     #logo
-    #yearsactive
-    #years with results of season
-    #current coach
-    #current gm
-    #current owner
-    #current roster (possible links to player models)
+    #years with results of season (record, playoff recap, mvp awards)
 
 class Player(models.Model) :
     name = models.CharField(max_length=30)
@@ -20,9 +19,15 @@ class Player(models.Model) :
     yearsExp = models.IntegerField(default=0)
     #stats by year with team played for, gp, gs, min, fg%, ft%, reb, ast, blk, stl, pts
     #awards for MVP, Finals MVP, all-NBA 1st team, all-defensive 1st team
+    #twitter
+    #photo
 
 class Year(models.Model) :
+    #year
     #all nba 1st team (players)
     #all nba defensive (players)
     #finals mvp (player)
     #champion (team)
+    #finals logo
+    #finals recap
+
