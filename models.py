77d2bd7e616f88a0dc5c057b07c487a8146e0708
Brian Tetzlaff <briantetzlaff@gmail.com>
2014-07-09 16:16:21 -0500

    added PlayerYear and TeamYear as additional classes

--- models.py
+++ models.py
@@ -10,7 +10,6 @@ class Team(models.Model) :
     owner = models.CharField(max_length=30)
     twitter = models.URLField(unique=True)
     logo = models.URLField(unique=True)
-    #years with results of season (record, playoff recap, mvp awards)
 
     def __unicode__(self) :
         return self.name
@@ -20,14 +19,8 @@ class Player(models.Model) :
     position = models.CharField(max_length=2)
     education = models.CharField(max_length=200)
     years_exp = models.IntegerField(default=0)
-    years_played = models.ManyToManyField(Year)
-    #stats by year with team played for, gp, gs, min, fg%, ft%, reb, ast, blk, stl, pts
-    mvp_years = models.ManyToManyField(Year)
-    finals_mvp_years = models.ManyToManyField(Year)
-    all_nba_years = models.ManyToManyField(Year)
-    all_def_years = models.ManyToManyField(Year)
     twitter = models.URLField(unique=True)
-    photo = models.URLField(unique=True)
+    photo = models.URLField(unique=True)   
 
     def __unicode__(self):
         return self.name
@@ -40,7 +33,37 @@ class Year(models.Model) :
     champion = models.ForeignKey(Team)
     finals_logo = models.URLField(unique=True)
     finals_recap = models.TextField()
-
+    
     def __unicode__(self):
         return self.year
+
+class PlayerYear(models.Model) :
+    year = models.ForeignKey(Year)
+    player = models.ForeignKey(Player)
+    team = models.ForeignKey(Team)
+    gp = models.IntegerField(default=0)
+    gs = models.IntegerField(default=0)
+    minpg = models.FloatField()
+    fgperc = models.FloatField()
+    ftperc = models.FloatField()
+    reb = models.IntegerField()
+    ast = models.IntegerField()
+    blk = models.IntegerField()
+    stl = models.IntegerField()
+    pts = models.FloatField()
+    mvp = models.BooleanField()
+    finals_mvp = models.BooleanField()
+    all_nba = models.BooleanField()
+    all_def = models.BooleanField()
+    
+    def __unicode__(self):
+        return (self.player.name, self.year.year)
+
+class TeamYear(models.Model) :
+    year = models.ForeignKey(Year)
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    playoffrecap = models.CharField() 
+
+    
 
