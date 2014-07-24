04acb7c1ccb4af6a7e9cd19f9483759da033a96f
Brian Tetzlaff <briantetzlaff@gmail.com>
2014-07-24 12:59:47 -0500

    fixed django model tests for updated models Fixes #21 Fixes #23

--- tests.py
+++ tests.py
@@ -1,12 +1,9 @@
-from django.utils import unittest
-from nba.models import Player, Team, Year
+from django.test import TestCase
+from django.db.utils import IntegrityError
+from nba.testapp1.models import Player, Team, Year
 
-class ModelsTestCase(unittest.TestCase):   
+class ModelsTestCase(TestCase):
 
-    #-----
-    # Test Team
-    #-----
-    
     # test creation (3 object tests)
     def test_Team1(self):
         lal = Team.objects.create(name="Lakers", location="Los Angelos", coach="vacant", gm="Mitch Kupchak", owner= "Jerry Buss", twitter="http://twitter.com/lakers", twitter_widget="9999", logo="http://imgur.com/lakers", finals_years="2005, 2006", mvps="Kobe Bryant", finals_mvps="Kobe Bryant")
@@ -22,9 +19,8 @@ class ModelsTestCase(unittest.TestCase):
         self.assertEqual(lal.mvps, "Kobe Bryant")
         self.assertEqual(lal.finals_mvps, "Kobe Bryant")
 
-
     def test_Player1(self):
-        lal = Team.objects.get(name="Lakers")
+        lal = Team.objects.create(name="Lakers", location="Los Angelos", coach="vacant", gm="Mitch Kupchak", owner= "Jerry Buss", twitter="http://twitter.com/lakers", twitter_widget="9999", logo="http://imgur.com/lakers", finals_years="2005, 2006", mvps="Kobe Bryant", finals_mvps="Kobe Bryant")
         kobe = Player.objects.create(name="Kobe Bryant", position="SG", education="HS", years_exp=15, twitter="http://twitter.com/kobebryant", twitter_widget="5000", nickname="Black Mamba", photo="http://imgur.com/kobe", highlights="http://youtube.com", current_team=lal, gp=1100, gs=1080, minpg=33.0, fgperc=0.450, ftperc=0.750, reb=4.5, ast=5.0, blk=0.5, stl=1.0, pts=27.5, finals_years="2003, 2005, 2006", finals_teams="Lakers", mvp_years="2006", finalsmvp_years="2006, 2008", allnba_years="2003, 2004", alldef_years="2000, 2002")
         self.assertEqual(kobe.name, "Kobe Bryant")
         self.assertEqual(kobe.position, "SG")
@@ -34,7 +30,7 @@ class ModelsTestCase(unittest.TestCase):
         self.assertEqual(kobe.twitter_widget, "5000")
         self.assertEqual(kobe.nickname, "Black Mamba")
         self.assertEqual(kobe.photo, "http://imgur.com/kobe")
-        self.assertEqual(kobe.hightlights, "http://youtube.com")
+        self.assertEqual(kobe.highlights, "http://youtube.com")
         self.assertEqual(kobe.current_team, lal)
         self.assertEqual(kobe.gp, 1100)
         self.assertEqual(kobe.gs, 1080)
@@ -49,14 +45,14 @@ class ModelsTestCase(unittest.TestCase):
         self.assertEqual(kobe.finals_years, "2003, 2005, 2006")
         self.assertEqual(kobe.finals_teams, "Lakers")
         self.assertEqual(kobe.mvp_years, "2006")
-        self.assertEqual(kobe.finalsmvp_years, "2008")
+        self.assertEqual(kobe.finalsmvp_years, "2006, 2008")
         self.assertEqual(kobe.allnba_years, "2003, 2004")
         self.assertEqual(kobe.alldef_years, "2000, 2002")
 
-    
+
     def test_Year1(self):
-        lal = Team.objects.get(name="Lakers")
-        kobe = Player.objects.get(name="Kobe Bryant")
+        lal = Team.objects.create(name="Lakers", location="Los Angelos", coach="vacant", gm="Mitch Kupchak", owner= "Jerry Buss", twitter="http://twitter.com/lakers", twitter_widget="9999", logo="http://imgur.com/lakers", finals_years="2005, 2006", mvps="Kobe Bryant", finals_mvps="Kobe Bryant")
+        kobe = Player.objects.create(name="Kobe Bryant", position="SG", education="HS", years_exp=15, twitter="http://twitter.com/kobebryant", twitter_widget="5000", nickname="Black Mamba", photo="http://imgur.com/kobe", highlights="http://youtube.com", current_team=lal, gp=1100, gs=1080, minpg=33.0, fgperc=0.450, ftperc=0.750, reb=4.5, ast=5.0, blk=0.5, stl=1.0, pts=27.5, finals_years="2003, 2005, 2006", finals_teams="Lakers", mvp_years="2006", finalsmvp_years="2006, 2008", allnba_years="2003, 2004", alldef_years="2000, 2002")
         y2006 = Year.objects.create(year="2006", finals_logo="http://imgur.com/2006", finals_recap="Kobe wins", highlights="http://youtube.com/2006", all_nba="Kobe, Nash", all_def="Kevin Garnett", reg_mvp=kobe, finals_mvp=kobe, champion=lal)
         self.assertEqual(y2006.year, "2006")
         self.assertEqual(y2006.finals_logo, "http://imgur.com/2006")
@@ -94,7 +90,6 @@ class ModelsTestCase(unittest.TestCase):
         self.assertEqual(tim.nickname, "")
         self.assertEqual(tim.photo, "http://imgur.com/tim")
         self.assertEqual(tim.highlights, "")
-        self.assertEqual(tim.current_team, 'none')
         self.assertEqual(tim.gp, 0)
         self.assertEqual(tim.gs, 0)
         self.assertEqual(tim.minpg, 0)
@@ -112,7 +107,7 @@ class ModelsTestCase(unittest.TestCase):
         self.assertEqual(tim.allnba_years, "")
         self.assertEqual(tim.alldef_years, "")
 
-   def test_Year2(self):
+    def test_Year2(self):
         y2007 = Year.objects.create(year="2007", finals_logo="http://imgur.com/2007", finals_recap= "")
         self.assertEqual(y2007.year, "2007")
         self.assertEqual(y2007.finals_logo, "http://imgur.com/2007")
@@ -120,55 +115,29 @@ class ModelsTestCase(unittest.TestCase):
         self.assertEqual(y2007.highlights, "")
         self.assertEqual(y2007.all_nba, "")
         self.assertEqual(y2007.all_def, "")
-        self.assertEqual(y2007.reg_mvp, 'none')
-        self.assertEqual(y2007.finals_mvp, 'none')
-        self.assertEqual(y2007.champion, 'none')
 
-    #-----
-    # test created (dummy) database
-    # ----
+    # test trying to create objects with conflicting unique fields
+    def test_Player3(self):
+        lal = Team.objects.create(name="Lakers", location="Los Angelos", coach="vacant", gm="Mitch Kupchak", owner= "Jerry Buss", twitter="http://twitter.com/lakers", twitter_widget="9999", logo="http://imgur.com/lakers", finals_years="2005, 2006", mvps="Kobe Bryant", finals_mvps="Kobe Bryant")
+        kobe = Player.objects.create(name="Kobe Bryant", position="SG", education="HS", years_exp=15, twitter="http://twitter.com/kobebryant", twitter_widget="5000", nickname="Black Mamba", photo="http://imgur.com/kobe", highlights="http://youtube.com", current_team=lal, gp=1100, gs=1080, minpg=33.0, fgperc=0.450, ftperc=0.750, reb=4.5, ast=5.0, blk=0.5, stl=1.0, pts=27.5, finals_years="2003, 2005, 2006", finals_teams="Lakers", mvp_years="2006", finalsmvp_years="2006, 2008", allnba_years="2003, 2004", alldef_years="2000, 2002")
+        try :
+            kobe2 = Player.objects.create(name="Kobe Bryant", position="SG", education="HS", years_exp=15, twitter="http://twitter.com/kobebryant", twitter_widget="5000", nickname="Black Mamba", photo="http://imgur.com/kobe", highlights="http://youtube.com", current_team=lal, gp=1100, gs=1080, minpg=33.0, fgperc=0.450, ftperc=0.750, reb=4.5, ast=5.0, blk=0.5, stl=1.0, pts=27.5, finals_years="2003, 2005, 2006", finals_teams="Lakers", mvp_years="2006", finalsmvp_years="2006, 2008", allnba_years="2003, 2004", alldef_years="2000, 2002")
+            self.assertFalse(kobe2.name == kobe.name)
+        except IntegrityError :
+            assert True
 
-    # Players
-    def test_fakeDB1(self):
-        kobe = Player.objects.get(name="Kobe Bryant")
-        tim = Player.objects.get(name="Tim Duncan")
+    def test_Team3(self):
+        sa = Team.objects.create(name="Spurs", location="San Antonio", coach="Pop", gm="rich guy 1", owner="rich guy 2", twitter="http://twitter.com/spurs", logo="http://imgur.com/spurs")
+        try :
+            sa2 = Team.objects.create(name="Spurs", location="San Antonio", coach="Pop", gm="rich guy 1", owner="rich guy 2", twitter="http://twitter.com/spurs", logo="http://imgur.com/spurs")
+            self.assertFalse(sa.name == sa2.name)
+        except IntegrityError :
+            assert True
 
-        # test uniques
-        if(kobe.nickname == tim.nickname) :
-            assert False
-        if(kobe.twitter == tim.twitter) :
-            assert False
-        if(kobe.photo == tim.photo) :
-            assert False
-        if(kobe.highlights == tim.highlights) :
-            assert False
-        
-        
-    # Teams
-    def test_fakeDB2(self):
-        lal = Team.objects.get(name="Lakers")
-        sa = Team.objects.get(name="Spurs")
-        
-        # test uniques
-        if(lal.location == sa.location):
-            assert False
-        if(lal.coach == sa.coach):
-            assert False
-        if(lal.gm == sa.gm):
-            assert False
-        if(lal.twitter == sa.twitter):
-            assert False
-        if(lal.logo = sa.logo):
-            assert False
-
-
-    # Years
-    def test_fakeDB3(self):
-        y2006 = Year.objects.get(year="2006")
-        y2007 = Year.objects.get(year="2007")
-
-        # test uniques
-        if(y2006.finals_logo == y2007.finals_logo):
-            assert False
-        if(y2006.highlights == y2007.highlights):
-            assert False
+    def test_Year3(self):
+        y2007 = Year.objects.create(year="2007", finals_logo="http://imgur.com/2007", finals_recap= "")
+        try :
+            y07 = Year.objects.create(year="2007", finals_logo="http://imgur.com/2007", finals_recap= "")
+            self.assertFalse(y2007.year == y07.year and y2007.finals_logo == y07.finals_logo)
+        except IntegrityError :
+            assert True
