9dcf944edb98872c28a17e44babdbd4f1652ab97
Brian Tetzlaff <briantetzlaff@gmail.com>
2014-07-24 09:19:16 -0500

    replaced models file with the one from python anywhere

--- models.py
+++ models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
-
 # Models for the NBA app
 
 class Team(models.Model) :
-    """
-    Contains information for a Team that is current and will not change
-    Each Team has multiple TeamYear's (One-To-Many)
-    """
     name = models.CharField(max_length=20, unique=True)
     location = models.CharField(max_length=30, unique=True)
     coach = models.CharField(max_length=30, unique=True)
     gm = models.CharField(max_length=30, unique=True)
-    owner = models.CharField(max_length=30)
+    owner = models.CharField(max_length=50)
     twitter = models.URLField(unique=True)
+    twitter_widget = models.CharField(max_length=500, default="")
     logo = models.URLField(unique=True)
-    finals_years = models.CharField(max_length=200)
-    mvps = models.CharField(max_length=200)
-    finals_mvps = models.CharField(max_length=200)
+    finals_years = models.CharField(max_length=200, default="")
+    mvps = models.CharField(max_length=200, default="")
+    finals_mvps = models.CharField(max_length=200, default="")
 
+    #years with results of season (record, playoff recap, mvp awards)
 
+    def __unicode__(self) :
+        return self.name
+    """
     def __str__(self) :
         return "name: " + self.name + "\n" \
                "location: " + self.location + "\n" \
@@ -28,41 +28,51 @@ class Team(models.Model) :
                "owner: " + self.owner + "\n" \
                "twitter: " + str(self.twitter) + "\n" \
                "logo: " + str(self.logo) + "\n"
+    """
 
-    class Meta:
-        app_label = 'nba'
+    #class Meta:
+    #    app_label = 'nba'
+
+
 
 class Player(models.Model) :
-    """
-    Contains information about a Player that is current and will not change
-    until the end of the next NBA season
-    """
     name = models.CharField(max_length=30, unique=True)
     position = models.CharField(max_length=2)
-    education = models.CharField(max_length=20)
+    education = models.CharField(max_length=40)
     years_exp = models.IntegerField(default=0)
+    #stats by year with team played for, gp, gs, min, fg%, ft%, reb, ast, blk, stl, pts
+    #awards for MVP, Finals MVP, all-NBA 1st team, all-defensive 1st team
     twitter = models.URLField(unique=True)
+    twitter_widget = models.CharField(max_length=500, default="")
+    nickname = models.CharField(max_length=40, default="")
     photo = models.URLField(unique=True)
+    highlights = models.URLField(unique=True, default="")
+    #team_logo = models.URLField(unique=True, default="")
+    current_team = models.ForeignKey('Team', related_name='current_team', to_field='name', default='none')
+
+    #ha = models.CharField(max_length=2, default="")
 
     #stats are career averages
     gp = models.IntegerField(default=0)
     gs = models.IntegerField(default=0)
-    minpg = models.FloatField()
-    fgperc = models.FloatField()
-    ftperc = models.FloatField()
-    reb = models.FloatField()
-    ast = models.FloatField()
-    blk = models.FloatField()
-    stl = models.FloatField()
-    pts = models.FloatField()
-    finals_years = models.CharField(max_length=200)
-    finals_teams = models.CharField(max_length=500)
-    mvp_years = models.CharField(max_length=200)
-    finalsmvp_years = models.CharField(max_length=200)
-    allnba_years = models.CharField(max_length=200)
-    alldef_years = models.CharField(max_length=200)
+    minpg = models.FloatField(default=0)
+    fgperc = models.FloatField(default=0)
+    ftperc = models.FloatField(default=0)
+    reb = models.FloatField(default=0)
+    ast = models.FloatField(default=0)
+    blk = models.FloatField(default=0)
+    stl = models.FloatField(default=0)
+    pts = models.FloatField(default=0)
+    finals_years = models.CharField(max_length=200, default="")
+    finals_teams = models.CharField(max_length=500, default="")
+    mvp_years = models.CharField(max_length=200, default="")
+    finalsmvp_years = models.CharField(max_length=200, default="")
+    allnba_years = models.CharField(max_length=200, default="")
+    alldef_years = models.CharField(max_length=200, default="")
 
-
+    def __unicode__(self):
+        return self.name
+    """
     def __str__(self) :
         return "name: " + self.name + "\n" \
                "position: " + self.position + "\n" \
@@ -70,59 +80,23 @@ class Player(models.Model) :
                "years of experience: " + self.years_exp + "\n" \
                "twitter: " + str(self.twitter) + "\n" \
                "photo: " + str(self.photo) + "\n"
-
-    class Meta:
-        app_label = 'nba'
-
+    """
 class Year(models.Model) :
-    """
-    Contains information about a certain Year
-    """
     year = models.CharField(max_length=4, unique=True)
-    all_nba = models.CharField(max_length=500)
-    all_def = models.CharField(max_length=500)
-    finals_mvp = models.CharField(max_length=50)
-    champion = models.CharField(max_length=50)
+    #all nba 1st team (players?)
+    #all nba defensive (players?)
+    #finals_mvp = models.ForeignKey(Player) (correct?)
+    #champion = models.ForeignKey(Team) (correct?)
     finals_logo = models.URLField(unique=True)
     finals_recap = models.TextField()
+    highlights = models.URLField(unique=True, default="")
+
+    all_nba = models.CharField(max_length=500, default="")
+    all_def = models.CharField(max_length=500, default="")
+    reg_mvp = models.ForeignKey('Player', related_name='reg_mvp', to_field='name', default='none')
+    finals_mvp = models.ForeignKey('Player', related_name='finals_mvp', to_field='name', default='none')
+    champion = models.ForeignKey('Team', related_name='champion', to_field='name', default='none')
 
 
-    class Meta:
-        app_label = 'nba'
-
-"""
-class PlayerYear(models.Model) :
-    
-    #Contains information/statistics about a Player during a certain Year
-    
-    year = models.ForeignKey(Year)
-    player = models.ForeignKey(Player)
-    team = models.ForeignKey(Team)
-    
-    mvp = models.BooleanField()
-    finals_mvp = models.BooleanField()
-    all_nba = models.BooleanField()
-    all_def = models.BooleanField()
-
-
-    class Meta:
-        app_label = 'nba'
-"""
-
-"""
-class TeamYear(models.Model) :
-    
-    #Contains information/statistics about a Team during a certain Year
-    
-    team = models.ForeignKey(Team)
-    year = models.ForeignKey(Year)
-    wins = models.IntegerField()
-    losses = models.IntegerField()
-    playoffrecap = models.TextField()
-
-
-    class Meta:
-        app_label = 'nba'
-
-"""
-
+    def __unicode__(self):
+        return self.year
